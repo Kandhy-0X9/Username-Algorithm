e7c0a5b04c6ee52d8da53a6245fd6c4e9d33589e
Kandhy Kasheng <kashengkandhy@gmail.com>
2025-05-15 12:57:10 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+from faker import Faker
+fake = Faker()
+
+#welcome the user
+
+print("Welcome User\n")
+
+#ask the user for his/her name
+
+name_E = input("What is your name? ").lower().replace(" ", "_")
+print()
+
+choice = input(" Do you want to use your real name? (Yes or No)").strip().lower()
+print()
+
+#create if statement 
+
+if choice == "yes":
+    randomNumber = str(random.randint(0, 100))
+    print(name_E+randomNumber)
+    print()
+
+elif choice == "no":   
+    print(fake.name())
+    print()
